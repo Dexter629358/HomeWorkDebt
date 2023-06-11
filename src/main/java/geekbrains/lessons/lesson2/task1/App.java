52db55a10ed5593ec0b442ab9d307c3f68e55278
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-06-11 17:43:20 +0200

    Implement task 1 from lesson 2

---
+++ src/main/java/geekbrains/lessons/lesson2/task1/App.java
@@ -0,0 +1,30 @@
+package geekbrains.lessons.lesson2.task1;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 4, 3, 2, 1};
+        boolean isPalindrome = palindrome(arr);
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not palindrome");
+        }
+    }
+
+    /**
+     * @param arr incoming string
+     * @return true if arr is a palindrome, otherwise false
+     * @apiNote Validates if array is a palindrome.
+     */
+    public static boolean palindrome(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            if (arr[i] != arr[arr.length - 1 - i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/geekbrains/lessons/lesson2/task1/README.md
@@ -0,0 +1,7 @@
+# Task 1
+
+An array of integers is given. Is it true that the array is symmetric (palindrome)?
+
+## Example
+
+- `[1, 2, 3, 4, 5, 4, 3, 2, 1]` -> `true`
